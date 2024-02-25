c02a56325433fb20c8e243b3490c2e2547dc8b4f
Tatyanahmuva <websightinno@gmail.com>
2024-02-25 15:06:57 +0300

    add

---
+++ Desktop/Muva Project/python project/user _input.py
@@ -0,0 +1,5 @@
+name =input('Enter your Name ..........')
+age= input('Enter your Age ..........')
+location = input('Enter your Location..........')
+
+print(f"My name is {name} and I am {age} years old and I live in {location}")
